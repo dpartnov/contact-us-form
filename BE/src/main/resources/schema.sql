814e8f184eadad4eef4f8d1b74a89a537ee77bed
Denys Partnov <dpartnov@outlook.com>
2022-07-03 16:17:56 +0200

    New table "requests" in schema file

--- BE/src/main/resources/schema.sql
+++ BE/src/main/resources/schema.sql
@@ -1,5 +1,18 @@
+DROP TABLE REQUESTS IF EXISTS;
 DROP TABLE REQUEST_TYPES IF EXISTS;
+
 CREATE TABLE REQUEST_TYPES (
     ID integer auto_increment primary key,
     LABEL varchar(100) not null
 );
+
+CREATE TABLE REQUESTS (
+    ID bigint auto_increment primary key,
+    REQUEST_TYPE_ID integer not null,
+    POLICY_NUMBER varchar(50) not null,
+    NAME varchar(50) not null,
+    SURNAME varchar(50) not null,
+    MESSAGE varchar(2000) not null,
+    foreign key (REQUEST_TYPE_ID) references REQUEST_TYPES(ID)
+);
+
