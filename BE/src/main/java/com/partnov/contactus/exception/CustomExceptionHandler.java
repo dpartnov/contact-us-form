d648af7761b061a746b6289d83d96b5d40b68d6c
Denys Partnov <dpartnov@outlook.com>
2022-07-03 16:17:13 +0200

    Custom exception handler

---
+++ BE/src/main/java/com/partnov/contactus/exception/CustomExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.partnov.contactus.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ * Custom exception handler for correctly exception processing.
+ */
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ValidationException.class)
+    public final ResponseEntity handleValidationException(Exception ex, WebRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity handleAllExceptions(Exception ex, WebRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ex.getMessage());
+    }
+}
