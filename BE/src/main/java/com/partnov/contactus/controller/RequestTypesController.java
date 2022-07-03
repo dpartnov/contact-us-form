6e4b2e54ddc43070650a162ff46107e162ee6349
Denys Partnov <dpartnov@outlook.com>
2022-07-03 17:46:36 +0200

    BE - Cross origin

--- BE/src/main/java/com/partnov/contactus/controller/RequestTypesController.java
+++ BE/src/main/java/com/partnov/contactus/controller/RequestTypesController.java
@@ -3,6 +3,7 @@ package com.partnov.contactus.controller;
 import com.partnov.contactus.service.RequestTypesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * REST Controller for Request type entity.
  */
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/requests/types")
 public class RequestTypesController {
--- BE/src/main/java/com/partnov/contactus/controller/RequestsController.java
+++ BE/src/main/java/com/partnov/contactus/controller/RequestsController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  *  REST controller for Requests.
  * 
  */
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/requests")
 public class RequestsController {
